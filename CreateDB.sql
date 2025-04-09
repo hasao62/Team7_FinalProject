f6b688a9ab9da7d3656f3f3ddde53a486f8199bd
Michael <Napoldin76@gmail.com>
2025-04-09 17:11:38 -0400

    createdb sql

---
+++ CreateDB.sql
@@ -0,0 +1,52 @@
+drop database if exists final_project;
+
+create database final_project;
+use final_project;
+
+create table if not exists Users(
+UserID int not null,
+Username varchar(255),
+Pass varchar(255),
+Email varchar(255),
+Bit boolean,
+primary key (UserID)
+);
+
+create table if not exists Commands(
+CommandID int not null,
+Command varchar(255),
+Name varchar(255),
+Promp varchar(255),
+Bit boolean,
+primary key (CommandID)
+);
+
+create table if not exists Transcripts(
+TranscriptID int not null,
+UserID int not null,
+AudioID int not null,
+CommandId int not null,
+Transcript text,
+Time_Log datetime,
+primary key (TranscriptID),
+foreign key (UserID) references Users(UserID),
+foreign key (CommandID) references Commands(CommandID)
+);
+
+create table if not exists Audio(
+AudioID int not null,
+TranscriptID int not null,
+UserID int not null,
+CommandId int not null,
+Audiocontent text,
+Time_Log datetime,
+primary key (AudioID),
+foreign key (UserID) references Users(UserID),
+foreign key (CommandID) references Commands(CommandID)
+);
+
+Alter TABLE Transcripts
+add CONSTRAINT FK_AudioID FOREIGN KEY (AudioID) REFERENCES Audio(AudioID);
+
+Alter TABLE Audio
+add CONSTRAINT FK_TranscriptID FOREIGN key (TranscriptID) REFERENCES Transcripts(TranscriptID);

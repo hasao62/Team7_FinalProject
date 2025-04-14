403b7c119b6ff7fd791fd86698e91e68f6be4d37
tthacker <rgt2031@email.vccs.com>
2025-04-14 15:51:44 -0400

    added comments to the SQL code

--- CreateDB.sql
+++ CreateDB.sql
@@ -1,8 +1,13 @@
+--Deletes the final_project DB it already exists
 drop database if exists final_project;
 
+--Creates the final_project DB
 create database final_project;
+
+--Selects the final_project DB to run the following code
 use final_project;
 
+--Creates the Users Table
 create table if not exists Users(
 UserID int not null,
 Username varchar(255),
@@ -12,6 +17,7 @@ Bit boolean,
 primary key (UserID)
 );
 
+--Creates the Commands Table
 create table if not exists Commands(
 CommandID int not null,
 Command varchar(255),
@@ -21,6 +27,7 @@ Bit boolean,
 primary key (CommandID)
 );
 
+--Creates the Transcipts Table
 create table if not exists Transcripts(
 TranscriptID int not null,
 UserID int not null,
@@ -33,6 +40,7 @@ foreign key (UserID) references Users(UserID),
 foreign key (CommandID) references Commands(CommandID)
 );
 
+--Creates the Audio Table
 create table if not exists Audio(
 AudioID int not null,
 TranscriptID int not null,
@@ -45,8 +53,10 @@ foreign key (UserID) references Users(UserID),
 foreign key (CommandID) references Commands(CommandID)
 );
 
+--Makes AudioID from the Audio table the FK in the Transcripts table
 Alter TABLE Transcripts
 add CONSTRAINT FK_AudioID FOREIGN KEY (AudioID) REFERENCES Audio(AudioID);
 
+--Makes TransciptID from the Transcripts table the FK in the Audio table
 Alter TABLE Audio
 add CONSTRAINT FK_TranscriptID FOREIGN key (TranscriptID) REFERENCES Transcripts(TranscriptID);
